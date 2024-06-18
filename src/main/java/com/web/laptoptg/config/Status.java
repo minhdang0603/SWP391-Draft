65d93f66118b4e7d8897affad90d15861641adea
minhdang0603 <ruabin1163@gmail.com>
2024-06-18 20:02:06 +0700

    them class status

---
+++ src/main/java/com/web/laptoptg/config/Status.java
@@ -0,0 +1,12 @@
+package com.web.laptoptg.config;
+
+public class Status {
+    public static final String ACTIVE = "active";
+    public static final String INACTIVE = "inactive";
+    public static final String PENDING = "pending";
+    public static final String PROCESSING = "processing";
+    public static final String RECEIVED = "received";
+    public static final String CANCELLED = "cancelled";
+    public static final String UNPAID = "unpaid";
+    public static final String PAID = "paid";
+}
